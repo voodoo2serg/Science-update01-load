b8f0faa4dbdec8b496e56de0b8bece5b466aa700
voodoo2serg <vodopetov01@gmail.com>
2026-04-05 01:09:02 +0300

    feat: add academic_os scaffold package

---
+++ backend/app/academic_os/__init__.py
@@ -0,0 +1,10 @@
+"""Academic Research OS scaffold package.
+
+This package is introduced in parallel to the legacy MVP routes.
+It provides the first code-level foundation for the future research-oriented
+platform described in the repository architecture documents.
+"""
+
+from .router import api_router
+
+__all__ = ["api_router"]
